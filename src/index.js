90cecef3e075806337c1ed86b111c5603e490146
Eddiano <anoeddi84@gmail.com>
2025-05-14 15:06:34 +0300

    Désactiver Gzip pour corriger HTTP/2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import '@fortawesome/fontawesome-free/css/all.min.css'; 
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
-
-reportWebVitals();
